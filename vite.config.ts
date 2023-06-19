d1e69ccdbeb6a2c629cc0a922099e07406dea239
lvzq <lvzq@kh-super.com>
2023-06-19 15:42:53 +0800

    feat: add CI

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/duration-statistics-excel',
   plugins: [
     vue(),
     Components({
